4d87e284bd445f5c2facb10d4c182e513279e3cf
Mitchell <mitchellvictoriano@gmail.com>
2021-04-16 03:48:14 -0700

    improved visual appearance of site

--- canvas.js
+++ canvas.js
@@ -1,51 +1,63 @@
 function line(ctx, x0, y0, x1, y1) {
-  ctx.moveTo(x0, y0);
-  ctx.lineTo(x1, y1);
-  ctx.stroke();
+	ctx.moveTo(x0, y0);
+	ctx.lineTo(x1, y1);
+	ctx.stroke();
 }
 
 class ClockCircle {
-  constructor(ctx, i, value,x, y, r, spaceColor,ringColor,numColor) {
-    this.ctx = ctx;
-    this.index = i;
-    this.value = value;
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.spaceColor = spaceColor;
-    this.ringColor = ringColor;
-    this.numColor = numColor;
-  }
+	constructor(ctx, i, value, x, y, r, spaceColor, ringColor, numColor) {
+		this.ctx = ctx;
+		this.index = i;
+		this.value = value;
+		this.x = x;
+		this.y = y;
+		this.r = r;
+		this.spaceColor = spaceColor;
+		this.ringColor = ringColor;
+		this.numColor = numColor;
+	}
 
-  onClick(mouse){
-    if (this.x + this.r > mouse.x &&
-        this.x - this.r < mouse.x &&
-        this.y + this.r > mouse.y &&
-        this.y - this.r < mouse.y) {
-          return true;
-    }
-  }
+	onClick(mouse) {
+		if (
+			this.x + this.r > mouse.x &&
+			this.x - this.r < mouse.x &&
+			this.y + this.r > mouse.y &&
+			this.y - this.r < mouse.y
+		) {
+			return true;
+		}
+	}
 
-  draw(){
-    circle(this.ctx, this.x, this.y, this.r, this.spaceColor,this.ringColor,this.numColor);
-    text(this.ctx, this.x, this.y, this.value, this.r, this.numColor);
-  }
+	draw() {
+		circle(
+			this.ctx,
+			this.x,
+			this.y,
+			this.r,
+			this.spaceColor,
+			this.ringColor,
+			this.numColor
+		);
+		text(this.ctx, this.x, this.y, this.value, this.r, this.numColor);
+	}
 }
 
-function circle(ctx, x, y, r, color,ringColor) {
-  ctx.beginPath();
-  ctx.arc(x, y, r, 0, 2 * Math.PI);
-  ctx.fillStyle = color;
-  ctx.fill();
-  ctx.strokeStyle = ringColor;
-  ctx.stroke();
+function circle(ctx, x, y, r, color, ringColor) {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2 * Math.PI);
+	ctx.fillStyle = color;
+	ctx.fill();
+	ctx.strokeStyle = ringColor;
+	ctx.stroke();
 }
-
 
 function text(ctx, x, y, text, size, color) {
-  ctx.font = size + "px Georgia";
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-  ctx.fillStyle = color;
-  ctx.fillText(text, x, y);
+	ctx.font = size + "px Georgia";
+	ctx.textAlign = "center";
+	ctx.strokeStyle = "black";
+	ctx.lineWidth = 2;
+	ctx.textBaseline = "middle";
+	ctx.fillStyle = color;
+	ctx.strokeText(text, x, y);
+	ctx.fillText(text, x, y);
 }
